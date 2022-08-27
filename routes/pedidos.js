3e58a605fca3045b2c1ed1dee884694e65ffd54d
facu09 <fcigliuti@gmail.com>
2022-08-27 20:20:39 -0300

    16vo cmmt: Para que rehaga el heroku app

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -3,7 +3,7 @@ const pedidosController = require("../controllers/pedidos");
 const router = express.Router();
 const auth = require("../middlewares/authorization");
 
-console.log("antes de entrar al route /api/pedidos")
+console.log("antes de entrar al route /api/pedidos");
 
 // Establecido por defecto x el app.js:  
 //  const usersRouter = require("./routes/pedidos") 
@@ -15,39 +15,39 @@ router.post("/", auth.authorizationForAllUser, pedidosController.createPedido);
 //   -->  /api/pedidos
 //Para el Usuario Logueado solo podrá consutar su pedido "actual" o alguno viejo 
 //Para el Usuario con Role ADMIN podra consultar cualquier pedido
-router.get("/idPedido/:idPedido", auth.authorizationForAllUser ,pedidosController.getById)
+router.get("/idPedido/:idPedido", auth.authorizationForAllUser ,pedidosController.getById);
 // Esto trae un json {} con todos los pedidos disponibles en la tabla ttPedidos
 //Comprobación Final de Permisos a hacer en Controller
 
 //   -->  /api/pedidos/xAsignar
 // Para ADMIN
-router.get("/xAsignar",  auth.authorizationForAdmin, pedidosController.getAllPedidosPorAsignar)
+router.get("/xAsignar",  auth.authorizationForAdmin, pedidosController.getAllPedidosPorAsignar);
 // Esto trae un json {} con todos los pedidos disponibles a ser Asignados a Cadete de la tabla ttPedidos
 
 //  --> /api/pedidos/xEntregar
 // Para ADMIN
-router.get("/xEntregar", auth.authorizationForAdmin, pedidosController.getAllPedidosAsignadosPorEntregar) 
+router.get("/xEntregar", auth.authorizationForAdmin, pedidosController.getAllPedidosAsignadosPorEntregar) ;
 // Esto treae todos los Pedidos por Entregar
 
 //  --> /api/pedidos/cancelByUs/idPedido/:idPedido
 //Para el Usuario Logueado solo podrá Cancelar su pedido "actual" o alguno otro pedido no entregado aun 
-router.put("/cancelByUs/idPedido/:idPedido", auth.authorizationForAllUser , pedidosController.cancelPedidoByIdByUs)
+router.put("/cancelByUs/idPedido/:idPedido", auth.authorizationForAllUser , pedidosController.cancelPedidoByIdByUs);
 // Esto Cancela el Pedido por el idPedido por el Usuario
 //Comprobación Final de Permisos a hacer en Controller
 
 //  --> /api/pedidos/cancelByHel/idPedido/:idPedido
 // Para ADMIN
-router.put("/cancelByHel/idPedido/:idPedido", auth.authorizationForAdmin , pedidosController.cancelPedidoByIdByHel)
+router.put("/cancelByHel/idPedido/:idPedido", auth.authorizationForAdmin , pedidosController.cancelPedidoByIdByHel);
 // Esto Cancela el Pedido por el idPedido por la Heladería.
 
 //  --> /api/pedidos/asignCadete/idPedido/:idPedido
 // Para ADMIN
-router.put("/asignCadete/idPedido/:idPedido", auth.authorizationForAdmin ,pedidosController.asignarPedidoACadeteById)
+router.put("/asignCadete/idPedido/:idPedido", auth.authorizationForAdmin ,pedidosController.asignarPedidoACadeteById);
 // Esto actualiza el pedido marcandolo como asignado a un Cadete, por el idPedido
 
 //  --> /api/pedidos/entregaPed/idPedido/:idPedido
 //// Para ADMIN o Para CADETE
-router.put("/entregaPed/idPedido/:idPedido", auth.authorizationForCadeteOrAdmin, pedidosController.entregaDePedidoById)
+router.put("/entregaPed/idPedido/:idPedido", auth.authorizationForCadeteOrAdmin, pedidosController.entregaDePedidoById);
 // Esto actualiza el pedido marcandolo como ENTREGADO, por el idPedido
 //Comprobación Final de Permisos a hacer en Controller si hubiera diferencais ente Cadete y ADMIN
 
